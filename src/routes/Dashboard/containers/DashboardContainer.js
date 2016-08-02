31d6fd849082c0adbcde4ba2756b5221da1cc0ef
przeor <kamil@przeorski.pl>
2016-08-02 11:52:54 +0200

    dashboard count per visit

--- src/routes/Dashboard/containers/DashboardContainer.js
+++ src/routes/Dashboard/containers/DashboardContainer.js
@@ -1,5 +1,6 @@
+import React from 'react'
 import { connect } from 'react-redux'
-import { increment, doubleAsync } from '../modules/dashboard'
+import { dashboardVisitIncrement, doubleAsync } from '../modules/dashboard'
 
 /*  This is a container component. Notice it does not contain any JSX,
     nor does it import React. This component is **only** responsible for
@@ -8,18 +9,32 @@ import { increment, doubleAsync } from '../modules/dashboard'
 
 import Dashboard from 'components/Dashboard'
 
-/*  Object of action creators (can also be function that returns object).
-    Keys will be passed as props to presentational components. Here we are
-    implementing our wrapper around increment; the component doesn't care   */
 
 const mapActionCreators = {
-  increment: () => increment(1),
+  dashboardVisitIncrement: () => dashboardVisitIncrement(1),
   doubleAsync
 }
 
 const mapStateToProps = (state) => ({
   dashboard: state.dashboard
 })
+
+
+class DashboardContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.dashboardVisitIncrement();
+  }
+
+  render () {
+    return (
+        <Dashboard {...this.props} />
+    );
+  }
+}
 
 /*  Note: mapStateToProps is where you should use `reselect` to create selectors, ie:
 
@@ -35,4 +50,4 @@ const mapStateToProps = (state) => ({
     Selectors are composable. They can be used as input to other selectors.
     https://github.com/reactjs/reselect    */
 
-export default connect(mapStateToProps, mapActionCreators)(Dashboard)
+export default connect(mapStateToProps, mapActionCreators)(DashboardContainer)
