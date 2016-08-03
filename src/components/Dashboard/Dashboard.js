8eef288af11d36d6485f00174f4b347ffd219508
przeor <kamil@przeorski.pl>
2016-08-03 17:26:54 +0200

    dashboard list v1

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import classes from './Dashboard.scss'
 
-export const Dashboard = (props) => (
+export const Dashboard = (props) => {
+
+  const listJSX = props.dashboard.dashboardItems.map((item, i) => {
+    return <h4 key={i}>{item.label}</h4>
+  })
+
+  return (
   <div>
     <h2 className={classes.dashboardContainer}>
       Dashboard visits:
       {' '}
       <span className={classes['dashboard--green']}>
-        {props.dashboard}
+        {props.dashboard.visitsCount}
       </span>
     </h2>
+    {listJSX}
   </div>
-)
+)}
 
 Dashboard.propTypes = {
-  dashboard: React.PropTypes.number.isRequired
+  dashboard: React.PropTypes.object.isRequired
 }
 
 export default Dashboard
