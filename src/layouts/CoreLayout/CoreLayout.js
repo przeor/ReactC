614abb151d0ce9c64c1bf7bc5ecdeaeb9042201a
przeor <kamil@przeorski.pl>
2016-08-31 14:05:12 +0200

    slideout (version1)

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -4,6 +4,12 @@ import classes from './CoreLayout.scss'
 import '../../styles/core.scss'
 import { connect } from 'react-redux'
 import { loginAsync } from '../../modules/session'
+import Slideout from 'slideout'
+
+// let's use global variable, because we need to work 
+// with it during the React Lifecycle happens
+// because the slideout lib works on the real browser's DOM (not virtual)
+let slideout; 
 
 const mapActionCreators = {
   loginAsync
@@ -24,21 +30,89 @@ class CoreLayout extends Component {
     this.handleLogin = this.handleLogin.bind(this)
   }
 
+  componentDidMount() {
+    slideout = new Slideout({
+      'panel': document.getElementById('panel'),
+      'menu': document.getElementById('menu'),
+      'padding': 256,
+      'tolerance': 70
+    })
+  }
+
   handleLogin(loginObj, e) {
     e.preventDefault()
     this.props.loginAsync(loginObj)
   }
 
   render () {
+
     return (
-      <div className='container text-center'>
-        <Header 
-          handleLogin={this.handleLogin} 
-          session={this.props.session} />
-        <div className={classes.mainContainer}>
+      <div>
+        <nav id="menu" className="menu">
+        <a href="https://github.com/mango/slideout" target="_blank">
+          <header className="menu-header">
+            <span className="menu-header-title">Tests</span>
+          </header>
+        </a>
+
+        <section className="menu-section">
+          <h3 className="menu-section-title">Docs</h3>
+          <ul className="menu-section-list">
+            <li><a href="https://github.com/mango/slideout#installation" target="_blank">Installation</a></li>
+            <li><a href="https://github.com/mango/slideout#usage" target="_blank">Usage</a></li>
+            <li><a href="https://github.com/mango/slideout#api" target="_blank">API</a></li>
+            <li><a href="https://github.com/mango/slideout#npm-scripts" target="_blank">npm-scripts</a></li>
+          </ul>
+        </section>
+
+        <section className="menu-section">
+          <h3 className="menu-section-title">Docs</h3>
+          <ul className="menu-section-list">
+            <li><a href="https://github.com/mango/slideout#installation" target="_blank">Installation</a></li>
+            <li><a href="https://github.com/mango/slideout#usage" target="_blank">Usage</a></li>
+            <li><a href="https://github.com/mango/slideout#api" target="_blank">API</a></li>
+            <li><a href="https://github.com/mango/slideout#npm-scripts" target="_blank">npm-scripts</a></li>
+          </ul>
+        </section>
+
+        <section className="menu-section">
+          <h3 className="menu-section-title">Docs</h3>
+          <ul className="menu-section-list">
+            <li><a href="https://github.com/mango/slideout#installation" target="_blank">Installation</a></li>
+            <li><a href="https://github.com/mango/slideout#usage" target="_blank">Usage</a></li>
+            <li><a href="https://github.com/mango/slideout#api" target="_blank">API</a></li>
+            <li><a href="https://github.com/mango/slideout#npm-scripts" target="_blank">npm-scripts</a></li>
+          </ul>
+        </section>
+
+        <section className="menu-section">
+          <h3 className="menu-section-title">Docs</h3>
+          <ul className="menu-section-list">
+            <li><a href="https://github.com/mango/slideout#installation" target="_blank">Installation</a></li>
+            <li><a href="https://github.com/mango/slideout#usage" target="_blank">Usage</a></li>
+            <li><a href="https://github.com/mango/slideout#api" target="_blank">API</a></li>
+            <li><a href="https://github.com/mango/slideout#npm-scripts" target="_blank">npm-scripts</a></li>
+          </ul>
+        </section>
+      </nav>
+
+      <main id="panel" className="panel">
+        <header className="panel-header">
+          <button onClick={() => { slideout.toggle() }} className="btn-hamburger js-slideout-toggle"></button>
+          <Header 
+            handleLogin={this.handleLogin} 
+            session={this.props.session} />
+        </header>
+
+        <section className="box">
           {this.props.children}
-        </div>
-      </div>)
+        </section>
+
+        <footer className="panel-footer">
+          <p>with <span className="heart">❤</span> by <a href="https://getmango.com/en" target="_blank">Mango</a></p>
+        </footer>
+      </main>
+    </div>)
 
     }
 }
