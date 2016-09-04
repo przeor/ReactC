9f077a858c2d5ef6e6af69a3be69e260a5371157
robrobkay51 <robert.kehoe@singingbottle.com>
2016-09-04 20:14:38 +0100

    React-ified CoreLayout
    Hamburger button not working - unable to include header without presentational errors

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -19,36 +19,15 @@ const mapStateToProps = (state) => ({
   session: state.session
 })
 
+//jsx components
 
-class CoreLayout extends Component {
-  static propTypes = {
-    children: PropTypes.object.isRequired
-  }
 
-  constructor(props) {
-    super(props)
-    this.handleLogin = this.handleLogin.bind(this)
-  }
+class Menu extends Component {
+render() {
 
-  componentDidMount() {
-    slideout = new Slideout({
-      'panel': document.getElementById('panel'),
-      'menu': document.getElementById('menu'),
-      'padding': 256,
-      'tolerance': 70
-    })
-  }
+return (
 
-  handleLogin(loginObj, e) {
-    e.preventDefault()
-    this.props.loginAsync(loginObj)
-  }
-
-  render () {
-
-    return (
-      <div>
-        <nav id="menu" className="menu">
+ <nav id="menu" className="menu">
         <a href="https://github.com/mango/slideout" target="_blank">
           <header className="menu-header">
             <span className="menu-header-title">Tests</span>
@@ -64,44 +43,21 @@ class CoreLayout extends Component {
             <li><a href="https://github.com/mango/slideout#npm-scripts" target="_blank">npm-scripts</a></li>
           </ul>
         </section>
-
-        <section className="menu-section">
-          <h3 className="menu-section-title">Docs</h3>
-          <ul className="menu-section-list">
-            <li><a href="https://github.com/mango/slideout#installation" target="_blank">Installation</a></li>
-            <li><a href="https://github.com/mango/slideout#usage" target="_blank">Usage</a></li>
-            <li><a href="https://github.com/mango/slideout#api" target="_blank">API</a></li>
-            <li><a href="https://github.com/mango/slideout#npm-scripts" target="_blank">npm-scripts</a></li>
-          </ul>
-        </section>
-
-        <section className="menu-section">
-          <h3 className="menu-section-title">Docs</h3>
-          <ul className="menu-section-list">
-            <li><a href="https://github.com/mango/slideout#installation" target="_blank">Installation</a></li>
-            <li><a href="https://github.com/mango/slideout#usage" target="_blank">Usage</a></li>
-            <li><a href="https://github.com/mango/slideout#api" target="_blank">API</a></li>
-            <li><a href="https://github.com/mango/slideout#npm-scripts" target="_blank">npm-scripts</a></li>
-          </ul>
-        </section>
-
-        <section className="menu-section">
-          <h3 className="menu-section-title">Docs</h3>
-          <ul className="menu-section-list">
-            <li><a href="https://github.com/mango/slideout#installation" target="_blank">Installation</a></li>
-            <li><a href="https://github.com/mango/slideout#usage" target="_blank">Usage</a></li>
-            <li><a href="https://github.com/mango/slideout#api" target="_blank">API</a></li>
-            <li><a href="https://github.com/mango/slideout#npm-scripts" target="_blank">npm-scripts</a></li>
-          </ul>
-        </section>
       </nav>
+   );
+  } 
+}
 
-      <main id="panel" className="panel">
+
+class Panel extends Component {
+render() {
+
+return (
+
+<main id="panel" className="panel">
         <header className="panel-header">
-          <button onClick={() => { slideout.toggle() }} className="btn-hamburger js-slideout-toggle"></button>
-          <Header 
-            handleLogin={this.handleLogin} 
-            session={this.props.session} />
+          <button onClick={ this.props.onClick } className="btn-hamburger js-slideout-toggle"></button> 
+         
         </header>
 
         <section className="box">
@@ -109,6 +65,66 @@ class CoreLayout extends Component {
         </section>
 
       </main>
+   );
+  } 
+}
+
+
+class CoreLayout extends Component {
+  static propTypes = {
+    children: PropTypes.object.isRequired
+  }
+
+  constructor(props) {
+    super(props)
+    this.handleLogin = this.handleLogin.bind(this)
+    
+    //defining state of CoreLayout menuSlideStatus
+    
+    this.state = {
+        collapsed: true,
+        notCollapsed: false, 
+        
+    };
+    
+  }
+
+
+  componentDidMount() {
+    slideout = new Slideout({
+      'panel': document.getElementById('panel'),
+      'menu': document.getElementById('menu'),
+      'padding': 256,
+      'tolerance': 70
+    
+  })
+  }
+
+
+  handleLogin(loginObj, e) {
+    e.preventDefault()
+    this.props.loginAsync(loginObj)
+  }
+
+//slidein/slideout functions
+
+  collapsed() {
+     this.setState.slideout.close();
+ }
+ 
+ 
+  notCollapsed() {
+     this.setState.slideout.open();
+ }
+
+
+  render () {
+
+    return (
+         <div> 
+    <Menu />
+    <Panel onClick={ this.collapsed === true ? this.notCollapsed : this.collapsed } />
+        
     </div>)
 
     }
