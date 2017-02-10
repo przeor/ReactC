07a5874646ba584a89be13973f2c18c391c0ef64
Kamil Przeorski <kamil.przeorski@gmail.com>
2017-02-10 17:51:06 +0100

    fetching with apollo plain

--- src/utils/apolloConfig.js
+++ src/utils/apolloConfig.js
@@ -1,33 +1,17 @@
-/**
- * Modify the config Scaphold URL to point to your specific app.
- * Find the URL at the top of the page on Scaphold.io once you've created an app.
- * Yup. It's that easy.
- */
-import fetch from 'isomorphic-fetch'
-
 const config = {
   scapholdUrl: 'https://us-west-2.api.scaphold.io/graphql/measly-jail'
 }
 
-function graphQLFetcher(graphQLParams) {
-  console.info('graphQLParams', graphQLParams)
+//          EXAMPLE #1.
+// *************************
+// *************************
+// *************************
 
-  return fetch(config.scapholdUrl, {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': localStorage.token ? 'Bearer ' + localStorage.token : ''
-    },
-    body: JSON.stringify(graphQLParams),
-  }).then(response => {
-  	console.info(response.json())
-  	// return response.json() 
-  })
-}
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import gql from 'graphql-tag'
 
-const operationName = 'GetAllDashboardItems'
 
-const query = `query ${operationName} {
+const query = gql`query GetAllDashboardItems {
   viewer {
     allDashboardItems  {
       edges {
@@ -40,8 +24,17 @@ const query = `query ${operationName} {
   }
 }`
 
-const q = { query: query, variables: 'null', operationName: operationName }
 
-graphQLFetcher(q)
 
-export default { config, graphQLFetcher }
+const opts = {uri: config.scapholdUrl}
+const networkInterface = createNetworkInterface(opts)
+const client = new ApolloClient({
+  networkInterface,
+});
+
+client.query({query}).then((results) => {
+  console.info('res', results)
+})
+
+
+export default { config }
